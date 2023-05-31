8f24ca5105582ef0d7039b06035c7356f7d07a31
tanayjoshi21 <tanayjoshi21122@gmail.com>
2023-05-31 09:10:46 +0530

    Complete Project

---
+++ app/src/main/java/com/tanayjoshi/android/imagetotext/MainActivity.java
@@ -0,0 +1,26 @@
+package com.tanayjoshi.android.imagetotext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button captureButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        captureButton = findViewById(R.id.idBtnCapture);
+        captureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,ScannerActivity.class));
+            }
+        });
+
+    }
+}
